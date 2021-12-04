c6353c3001090284230b33cd63b54d16693e14f6
Janya Joshi <janyajoshi18@gmail.com>
2021-12-04 19:26:59 +0530

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
 
-	// @Override
-    // protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    //     return application.sources(WebappApplication.class);
-    // }
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
