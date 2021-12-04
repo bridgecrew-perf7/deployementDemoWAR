4587a3c759a7f7c7aa3cc4a43f3ae418588b0d63
Janya Joshi <janyajoshi18@gmail.com>
2021-12-04 16:50:58 +0530

    initiated

---
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/sample")
+    public String sample(){
+        return "sample";
+    }
+}
