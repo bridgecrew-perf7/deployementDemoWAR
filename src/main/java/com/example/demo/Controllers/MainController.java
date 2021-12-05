4cb1c41ce7b99997c413b393b8a43543a88b2ca3
Janya Joshi <janyajoshi18@gmail.com>
2021-12-05 09:40:45 +0530

    added /env

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -1,15 +1,32 @@
 package com.example.demo.Controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    @Autowired
+    private Environment env;
+
     @GetMapping("/")
     @ResponseBody
     public String home() {
         return "Home";
+    }
+
+    @GetMapping("/env")
+    @ResponseBody
+    public Map<String, String> env() {
+        Map<String, String> data = new HashMap<>();
+        data.put("port", env.getProperty("server.port"));
+        data.put("variable1", env.getProperty("variable1"));
+        return data;
     }
 
     @GetMapping("/test")
